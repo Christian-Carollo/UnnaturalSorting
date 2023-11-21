17626699eaeec6c2849dac3769f424cdcf56152e
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-21 17:20:36 +0100

    Aggiungere i file di progetto.

---
+++ UnnaturalSorting/Program.cs
@@ -0,0 +1,6 @@
+﻿Random random = new Random();
+
+Console.WriteLine("Lista di interi random con ordine personalizzato in base alla lunghezza dei caratteri:\n");
+
+Enumerable.Range(1, 1000).Select(numeriCasuali => random.Next(1000)).OrderBy(numeriOrdinati => numeriOrdinati.ToString().Length).ToList().ForEach(numero => Console.WriteLine(numero));
+
